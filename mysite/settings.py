e992bd0e3bdefc210f7aad7035bf5ed54899f2f4
Yuki Mizuno <muscular.mizuno@gmail.com>
2024-02-03 13:28:03 +0900

    Finished part2

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "ja"
+LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "ja"
 
 TIME_ZONE = "Asia/Tokyo"
 
